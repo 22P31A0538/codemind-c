a6f78c93f97f023235bae1dcde9c273042ed14c6
veera .madhurya <120097454+22P31A0538@users.noreply.github.com>
2022-12-23 05:34:19 +0530

    #include<stdio.h> int main() {     int n,i,a,b,c,d,e,f;     scanf("%d",&n);     a=n%10;     b=n/10;     c=b%10;     d=b/10;     e=c%10;     f=e/10;     if(a%2==0&&c%2==0&&e%2==0)printf("Even");     else if(a%2==1&&c%2==1&&e%2==1)printf("Odd");     else printf("Mixed"); }

---
+++ Even_Odd_Mixed.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a,b,c,d,e,f;
+    scanf("%d",&n);
+    a=n%10;
+    b=n/10;
+    c=b%10;
+    d=b/10;
+    e=c%10;
+    f=e/10;
+    if(a%2==0&&c%2==0&&e%2==0)printf("Even");
+    else if(a%2==1&&c%2==1&&e%2==1)printf("Odd");
+    else printf("Mixed");
+}
+    
+    
