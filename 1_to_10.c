9a26b40ffff4ed8a285142ca8e524a7122659cfd
veera .madhurya <120097454+22P31A0538@users.noreply.github.com>
2022-12-26 20:35:35 +0530

    #include<stdio.h> int main() {     int n,i,s=0;     scanf("%d",&n);     for(i=1;i<=10;i++)     {     printf("%d ",i);     } }

--- 1_to_10.c
+++ 1_to_10.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-int main(){
-    int n,i;
+int main()
+{
+    int n,i,s=0;
     scanf("%d",&n);
     for(i=1;i<=10;i++)
     {
-        printf("%d ",i);
-    
+    printf("%d ",i);
     }
 }
