90133fd1e3444d7e7f4f802624d0baf13158e346
veera .madhurya <120097454+22P31A0538@users.noreply.github.com>
2023-01-03 20:10:07 +0530

    #include<stdio.h> int main() {     int a,b,i,gcd;     scanf("%d%d",&a,&b);     for(i=1;i<=a&&i<=b;i++)     {     if(a%i==0&&b%i==0)        {         gcd=i;        }     }     printf("%d",gcd); }

--- Greatest_Common_Divisor.c
+++ Greatest_Common_Divisor.c
@@ -5,10 +5,10 @@ int main()
     scanf("%d%d",&a,&b);
     for(i=1;i<=a&&i<=b;i++)
     {
-        if(a%i==0&&b%i==0)
-        {
-            gcd=i;        
-        }
+    if(a%i==0&&b%i==0)
+       {
+        gcd=i;
+       }
     }
     printf("%d",gcd);
 }
