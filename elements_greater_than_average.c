cfd6e67e0e6ba327eee130f0b25eb705c17b1b6c
veera .madhurya <120097454+22P31A0538@users.noreply.github.com>
2023-01-12 21:41:38 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,s=0,c=0,k;     scanf("%d",&n);     int a[i];     for(i=1;i<=n;i++)     {         scanf("%d",&a[i]);
        } 
        for(i=1;i<=n;i++)
        {
            s=s+a[i];
            } 
            k=(s/n);
            for(i=1;i<=n;i++)
            {
                if(a[i]>=k)
                c++;
                
                
                    
            }
            printf("%d",c); 
             
    }

---
+++ elements_greater_than_average.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{ 
+    int n,i,s=0,c=0,k;
+    scanf("%d",&n); 
+    int a[i]; 
+    for(i=1;i<=n;i++)
+    { 
+        scanf("%d",&a[i]); 
+        
+    } 
+    for(i=1;i<=n;i++)
+    {
+        s=s+a[i];
+        } 
+        k=(s/n);
+        for(i=1;i<=n;i++)
+        {
+            if(a[i]>=k)
+            c++;
+            
+            
+                
+        }
+        printf("%d",c); 
+         
+}
+            
